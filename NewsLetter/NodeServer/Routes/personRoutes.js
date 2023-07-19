4e84493909345b169aa28c4642d9103252ff4595
yahya12261 <64883809+yahya12261@users.noreply.github.com>
2023-07-19 18:18:30 +0300

    Delete NewsLetter/NodeServer directory

--- NewsLetter/NodeServer/Routes/personRoutes.js
+++
@@ -1,9 +0,0 @@
-const exprees = require("express");
-const router = exprees.Router();
-const multer = require("multer");
-const upload = multer();
-const { getPersonFromAccountId } = require("../Services/personServices");
-const { isLoggedIn } = require("../middleware/users");
-router.get("/getPerson", isLoggedIn, getPersonFromAccountId);
-
-module.exports = router;
